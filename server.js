49b5a025e5f667c140caedd29d48a5058e19d688
Thangavel <indianthanga@gmail.com>
2018-10-02 20:50:16 +0530

    Added Media Queries Still in Progress

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname+'/dist/rp-digital/'));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log("RP Digital Running..."));
